526cc7ca79d1bf95475526fde88eb2906ad3d8e2
PhForty <37180056+PhForty@users.noreply.github.com>
2021-03-18 14:49:33 +0100

    Initial Upload

---
+++ song_game.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 18. Mrz 2021 um 14:47
+-- Server-Version: 10.4.6-MariaDB
+-- PHP-Version: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `song_game`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `songs`
+--
+
+CREATE TABLE `songs` (
+  `youtube_link` text COLLATE latin1_german1_ci NOT NULL,
+  `timestamp` date NOT NULL DEFAULT current_timestamp(),
+  `ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
+
+--
+-- Daten für Tabelle `songs`
+--
+
+INSERT INTO `songs` (`youtube_link`, `timestamp`, `ID`) VALUES
+('https://www.youtube.com/watch?v=RRsq9apr5QY', '2021-03-18', 5),
+('https://www.youtube.com/watch?v=RRsq9apr5QY', '2021-03-18', 6),
+('https://www.youtube.com/watch?v=RRsq9apr5QY', '2021-03-18', 7),
+('https://www.youtube.com/watch?v=RRsq9apr5QY', '2021-03-18', 8);
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `songs`
+--
+ALTER TABLE `songs`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `songs`
+--
+ALTER TABLE `songs`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
